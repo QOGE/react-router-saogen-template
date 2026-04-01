4200d4d0fab0486f24683bc74ac7d1108559bba9
SAOGEN <107507428+QOGE@users.noreply.github.com>
2026-04-01 09:26:28 +0100

    5657698365789770
    55678890

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,5 +1,5 @@
 import logoDark from "./saogen-logo.png";
-import logoLight from "./logo-light.svg";
+import logoLight from "./saogen-logo-light.png";
 
 export function Welcome({ message }: { message: string }) {
 	return (
@@ -21,13 +21,13 @@ export function Welcome({ message }: { message: string }) {
 				</header>
 				
      
-        <h1>SAOGEN</h1>
+        <h1>Symbiotic Autonomous Organization</h1>
       
     
 				<div className="max-w-[300px] w-full space-y-6 px-4">
 					<nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
 						<p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-							Symbiotic Autonomous Organization built on the QOGE blockchain.
+							built on the QOGE blockchain.
 						</p>
 						<p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
 							Hyper-optimization, shared intellectual property, and long-term
