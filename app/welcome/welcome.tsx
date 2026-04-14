eee940628033063bf5724fe2de76dbfb1cb461be
SAOGEN <107507428+QOGE@users.noreply.github.com>
2026-04-14 22:28:19 +0100

    Update welcome.tsx

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -28,9 +28,6 @@ export function Welcome({ message }: { message: string }) {
     </main>
 				<div className="max-w-[300px] w-full space-y-6 px-4">
 					<nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-						<p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-							 <button class="bg-blue-500 ...">What&apos;s next?</button>
-						</p>
 						
 						<ul>
 							{resources.map(({ href, text, icon }) => (
